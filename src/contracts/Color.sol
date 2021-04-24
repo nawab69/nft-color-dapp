6640dc6b595d90061b3ede349c8180585312f86d
nawab69 <nawabkhairuzzaman@gmail.com>
2021-04-25 03:22:04 +0600

    Created smart contract and frontend

---
+++ src/contracts/Color.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.5.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+
+contract Color is ERC721Full{
+
+      string[] public colors;
+      mapping(string => bool) _colorExists;
+       
+       constructor() ERC721Full("Color","COLOR") public {
+
+       }
+
+       function mint(string memory _color) public {
+             // Require unique color
+
+             require(!_colorExists[_color]);
+            //Color - add it
+             uint _id = colors.push(_color);
+            // Call the mint function
+            _mint(msg.sender, _id);
+           
+             // Color -track it
+
+              _colorExists[_color] = true;
+
+
+       }
+}
